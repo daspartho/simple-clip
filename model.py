e096c165ba492195c4347898c83d4eefbd5e0398
daspartho <parthodas6176@gmail.com>
2023-02-21 04:18:35 +0530

    text encoder

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
 from torch import nn
-from transformers import ViTConfig, ViTModel, ViTImageProcessor
+from transformers import (
+    ViTConfig, 
+    ViTModel, 
+    ViTImageProcessor,
+    BertConfig,
+    BertModel,
+    BertTokenizer,
+)
 
 class ImageEncoder(nn.Module):
 
@@ -20,5 +27,25 @@ class ImageEncoder(nn.Module):
     def forward(self, image):
         input = self.image_processor(image, return_tensors="pt")
         output = self.model(input.pixel_values)
+        last_hidden_state = output.last_hidden_state
+        return last_hidden_state[:, 0, :]
+
+class TextEncoder(nn.Module):
+
+    def __init__(self, hidden_size=768, num_hidden_layers=12, num_attention_heads=12):
+        super().__init__()
+
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        
+        config = BertConfig(
+            hidden_size=hidden_size,
+            num_hidden_layers=num_hidden_layers,
+            num_attention_heads=num_attention_heads
+        )
+        self.model = BertModel(config)
+
+    def forward(self, text):
+        input = self.tokenizer(text, return_tensors="pt", padding="max_length", truncation=True)
+        output = self.model(input.input_ids)
         last_hidden_state = output.last_hidden_state
         return last_hidden_state[:, 0, :]
