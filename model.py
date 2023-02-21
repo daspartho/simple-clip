eb129225cbe994f97aa0df6f395087d505870f33
daspartho <parthodas6176@gmail.com>
2023-02-21 16:21:41 +0530

    clip model

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
+import torch
 from torch import nn
+import torch.nn.functional as F
 from transformers import (
     ViTConfig, 
     ViTModel, 
@@ -49,3 +51,52 @@ class TextEncoder(nn.Module):
         output = self.model(input.input_ids)
         last_hidden_state = output.last_hidden_state
         return last_hidden_state[:, 0, :]
+
+class CLIPModel(nn.Module):
+
+    def __init__(
+        self, 
+        dim_latent=768,
+        image_size=224, 
+        patch_size=16, 
+        image_encoder_hidden_size=768, 
+        image_encoder_num_hidden_layers=12, 
+        image_encoder_num_attention_heads=12, 
+        text_encoder_hidden_size=768, 
+        text_encoder_num_hidden_layers=12, 
+        text_encoder_num_attention_heads=12,
+        ):
+        super().__init__()
+
+        self.image_encoder = ImageEncoder(
+            image_size=image_size, 
+            patch_size=patch_size, 
+            hidden_size=image_encoder_hidden_size, 
+            num_hidden_layers=image_encoder_num_hidden_layers, 
+            num_attention_heads=image_encoder_num_attention_heads,
+            )
+
+        self.text_encoder = text_encoder(
+            hidden_size=text_encoder_hidden_size, 
+            num_hidden_layers=text_encoder_num_hidden_layers, 
+            num_attention_heads=text_encoder_num_attention_heads,
+        )
+
+        self.image_projection = nn.Linear(image_encoder_hidden_size, dim_latent, bias=False)
+        self.text_projection = nn.Linear(text_encoder_hidden_size, dim_latent, bias=False)
+
+    def forward(self, text, image):
+
+        image_features = self.image_encoder(image)
+        image_latents = self.image_projection(image_features)
+
+        text_features = self.text_encoder(text)
+        text_latents = self.text_projection(text_features)
+
+        logits = image_latents @ text_latents.T
+        labels = torch.arange(logits.size(dim=0))
+        
+        loss_image = F.cross_entropy(logits, labels)
+        loss_text = F.cross_entropy(logits.T, labels)
+        loss = (loss_image + loss_text)/2
+        return loss
